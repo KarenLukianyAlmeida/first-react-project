9d5150c282a28c73db2878ed69d674b16f1f6c43
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-12-03 17:30:22 +0000

    Cria arquivo separa do de data

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,7 @@ import Title from './components/title';
 
 import './App.css';
 
-const booksList = [
-  {
-    title: 'Dom Casmurro',
-    pages: 200,
-    isRead: true,
-    isFavorite: false,
-  },
-  {
-    title: 'Ladrão de Raios',
-    pages: 300,
-    isRead: true,
-    isFavorite: true,
-  },
-  {
-    title: 'Vidas Secas',
-    pages: 136,
-    isRead: false,
-    isFavorite: false,
-  },
-  {
-    title: 'Mar de Monstros',
-    pages: 300,
-    isRead: true,
-    isFavorite: false,
-  },
-  {
-    title: 'O Segredo dos Dinamarqueses',
-    pages: 350,
-    isRead: false,
-    isFavorite: false,
-  },
-];
+import { booksList } from './data';
 
 function App() {
   return (
---
+++ src/data.ts
@@ -0,0 +1,32 @@
+export const booksList = [
+  {
+    title: 'Dom Casmurro',
+    pages: 200,
+    isRead: true,
+    isFavorite: false,
+  },
+  {
+    title: 'Ladrão de Raios',
+    pages: 300,
+    isRead: true,
+    isFavorite: true,
+  },
+  {
+    title: 'Vidas Secas',
+    pages: 136,
+    isRead: false,
+    isFavorite: false,
+  },
+  {
+    title: 'Mar de Monstros',
+    pages: 300,
+    isRead: true,
+    isFavorite: false,
+  },
+  {
+    title: 'O Segredo dos Dinamarqueses',
+    pages: 350,
+    isRead: false,
+    isFavorite: false,
+  },
+];
